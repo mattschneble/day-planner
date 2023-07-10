423ee6426af7eb20ca74c36e59c76d2e681ad9cb
mattschneble <mattschneble@gmail.com>
2023-07-10 09:58:20 -0700

    added iterative for loop to look through the time blocks and apply correct class

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
-    //define global variables
-    var currentDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
-    var currentTime = dayjs().hour();
+    //define variables for use later
+    var todayDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+    var todayTime = dayjs().hour();
     var hourBlock = $(".hour-block");
 
-    // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. 
+    //created event listener for the save buttons to save the user input to localStorage
     hourBlock.children(".saveBtn").on("click", function () {
         var textInput = $(this).siblings(".entertext").val();
         localStorage.setItem($(this).parent().attr("id"), textInput);
@@ -15,18 +14,18 @@ $(function () {
     //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
 
     //Plan of Attack #2
-    //select child elements through use of jQuery
-    //utilize dayjs() to get the current hour ONLY
     //create a for loop to iterate through the time blocks
-    //expected for loop:
-        //for (i=9; i<18; i++) //uses 24 hour clock
-    //inside the for loop above:
-        //a. use if statement to compare the actual hour vs the hour block to see if the hour is in the past/present/future
-        //b. if the current hour is less the hour block, it will be in the future
-        //c. if the current hour is equal to the hour block, it will be in the present
-        //d. if the current hour is greater than the hour block, it will be in the past
-    //remove the listed class in the html and add the class that corresponds to the if statement
-    //can utilize the setInterval() method to update the time
+    for (var i = 0; i < hourBlock.length; i++) {
+        //create a variable to get the current hour block to be used to iterate through the hour blocks in the for loop to compare to the current time and apply the correct class
+        var currentHour = i + 9;
+        if (currentHour < todayTime) {
+            $(hourBlock[i]).addClass("past");
+        } else if (currentHour === todayTime) {
+            $(hourBlock[i]).addClass("present");
+        } else {
+            $(hourBlock[i]).addClass("future");
+            }
+        }
 
 
     // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
