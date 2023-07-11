9770d5976f798b08cb50921b93ae6632f543b9e5
mattschneble <mattschneble@gmail.com>
2023-07-11 08:12:21 -0700

    added for loop to make changes to hour blocks

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-$(function () {
-    //define variables for use later
-    var todayDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
-    var todayTime = dayjs().hour();
+//define variables for use later
+var todayDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+var todayTime = dayjs().hour();
+
+//making sure the page doesn't run until the DOM is ready
+
+$(document).ready(function () {
     var hourBlock = $(".hour-block");
 
     //created event listener for the save buttons to save the user input to localStorage
@@ -10,13 +13,9 @@ $(function () {
         localStorage.setItem($(this).parent().attr("id"), textInput);
     });
 
-    // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
-    //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-
-    //Plan of Attack #2
-    //create a for loop to iterate through the time blocks
+    //create a for loop to run through the time blocks
     for (var i = 0; i < hourBlock.length; i++) {
-        //create a variable to get the current hour block to be used to iterate through the hour blocks in the for loop to compare to the current time and apply the correct class
+        //create a variable to get the current hour block to be used to run through the hour blocks in the for loop to compare to the current time and apply the correct class
         var currentHour = i + 9;
         if (currentHour < todayTime) {
             $(hourBlock[i]).addClass("past");
@@ -28,21 +27,14 @@ $(function () {
         }
 
 
-    // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+    //create a for loop to run through the hour blocks and make changes to the hour blocks that are in the past, present, and future
+    for(var i = 9; i <= 17; i++){
+        var enterText = hourBlock.eq(i-9).sibling(".entertext");
+        //retrieve the inputed text from localStorage and display it in the correct hour block
+        enterText.val(localStorage.getItem("hour-block-" + i));
+    }
 
-    //Plan of Attack #3
-    //use getItem to retrieve the saved input value from localStorage for each hour block
-    //create a for loop to iterate through the time blocks
-        //make sure to check localStorage within the loop
-        localStorage.getItem()
-        //need to display items that are in localStorage to the corresponding hour block
-        //use id selectors to get the info in the textarea
-
-    // TODO: Add code to display the current date in the header of the page.
-
-    //Plan of Attack #4
-    //create a variable for dayjs() to get the current date (can optionally add time)
-    // $('#currentDay).text(variable created above) to display the current date in the header of the page
+    $('#todayDate').text(todayDate);
 
   });
   
